f87e4b86a008e4538488ca4530c634e32c338eff
Sanket Telunagi <telunagisanket@gmail.com>
2026-05-16 10:19:54 +0530

    new problems

---
+++ solutions/leetcode/LC_154_Find_Minimum_In_Rotated_Sorted_Array_II.py
@@ -0,0 +1,8 @@
+# Solution for 154. Find Minimum in Rotated Sorted Array II
+# Platform: LeetCode
+# Date: 2026-05-16
+#
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
