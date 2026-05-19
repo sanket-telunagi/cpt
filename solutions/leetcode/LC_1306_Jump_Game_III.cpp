8e8f8df43d5cae48ecc4fdd39473c10211820176
Sanket Telunagi <telunagisanket@gmail.com>
2026-05-19 18:49:30 +0530

    new problems

---
+++ solutions/leetcode/LC_1306_Jump_Game_III.cpp
@@ -0,0 +1,17 @@
+// Solution for 1306. Jump Game III
+// Platform: LeetCode
+// Date: 2026-05-18
+//
+bool solve() {
+
+}
+
+int main() {
+
+    int n ;
+    cin >> n ;
+    int arr[n] ;
+    for
+
+    return 0 ;
+}
---
+++ solutions/leetcode/LC_2540_Minimum_Common_Value.py
@@ -0,0 +1,14 @@
+# Solution for 2540. Minimum Common Value
+# Platform: LeetCode
+# Date: 2026-05-19
+#
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        for num in nums2 : 
+            if num in nums1 : 
+                return num
+
+        return -1
