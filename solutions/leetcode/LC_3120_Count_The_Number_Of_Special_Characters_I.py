d20d8e982efc5960c896bca39c3655b9c3d9a465
Sanket Telunagi <telunagisanket@gmail.com>
2026-05-26 22:56:30 +0530

    3120. Count the Number of Special Characters I

---
+++ solutions/leetcode/LC_3120_Count_The_Number_Of_Special_Characters_I.py
@@ -0,0 +1,22 @@
+# Solution for 3120. Count the Number of Special Characters I
+# Platform: LeetCode
+# Date: 2026-05-26
+#
+
+
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        hash = set(list(word))
+        hash2 = [False] * 26
+        count = 0
+        for i in word:
+            if not hash2[ord("a") - ord(i.lower())]:
+                if i.isupper():
+                    if i.lower() in hash:
+                        count += 1
+                else:
+                    if i.upper() in hash:
+                        count += 1
+                hash2[ord("a") - ord(i.lower())] = True
+
+        return count
